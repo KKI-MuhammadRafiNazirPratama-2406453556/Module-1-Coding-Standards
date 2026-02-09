2141f82731264e7b6f316c4c601e2193be10ea20
sirpratama <mnazirpratama@gmail.com>
2026-02-09 21:24:35 +0700

    Edited ProductRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -42,4 +42,7 @@ public class ProductRepository {
         }
         return null;
     }
+    public void delete(String productId) {
+        productData.removeIf(product -> product.getProductId().equals(productId));
+    }
 }
